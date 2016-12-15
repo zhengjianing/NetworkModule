4422a254fa0a533ae10b5896e67269c1ee6a3a56
jianing <jianingzheng@gmail.com>
2016-12-15 13:13:37 +0800

    Create user model.

---
+++ NetworkModule/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  NetworkModule
+//
+//  Created by Jianing Zheng on 12/15/16.
+//  Copyright © 2016 Jianing Zheng. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let name: String
+    let message: String
+    
+    init?(data: Data) {
+        
+        guard let object = try? JSONSerialization.jsonObject(with: data, options: []), let obj = object as? [String: Any] else {
+            return nil
+        }
+        
+        guard let name = obj["name"] as? String else {
+            return nil
+        }
+        
+        guard let message = obj["message"] as? String else {
+            return nil
+        }
+        
+        self.name = name
+        self.message = message
+    }
+
+}
